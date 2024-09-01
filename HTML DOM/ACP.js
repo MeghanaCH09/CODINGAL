d06f735b2d7ec49b61faf1b0dded15ea8507bc51
MeghanaCH09 <meghanacheruvu@gmail.com>
2024-09-01 20:07:11 +0200

    ACP

---
+++ HTML DOM/ACP.js
@@ -0,0 +1,4 @@
+function myfunction(){
+    document.getElementById("result").innerHTML = document.getElementById("demo").firstChild.nodeValue;
+    document.getElementById("result1").innerHTML = document.getElementById("dmeo").chilNodes[0].nodeValue;
+}
