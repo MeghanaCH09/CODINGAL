465b5f1b1f7930de452d79d7a7e5d535044f4bac
MeghanaCH09 <meghanacheruvu@gmail.com>
2024-10-19 13:29:30 +0200

    opandcomp

---
+++ Meghana/PYTHON/Operators and Data Types/operator and comparison.py
@@ -0,0 +1,24 @@
+a=3
+b=10
+
+print("The first number is ", a)
+print("The second number is ", b)
+
+print("The addition of both numbers: ", a+b)
+print("The substraction of both numbers: ", a-b)
+print("The division of both numbers: ", a/b)
+print("The multiplication of both numbers: ", a*b)
+print("The floor division of both numbers: ", a//b)
+print("The modules of both numbers: ", a%b)
+print("The exponential of the first number: ", a**2)
+print("The exponential of the second number: ", b**2)
+
+print(a==b)
+print(a!=b)
+print(a<b)
+print(a>b)
+print(a<=b)
+print(a>=b)
+
+mixedstmt=a+b*2/4**2
+print(mixedstmt)
