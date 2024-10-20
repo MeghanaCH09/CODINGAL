07236025cfc126615acc7e3d82211f6d6c94ade9
MeghanaCH09 <meghanacheruvu@gmail.com>
2024-10-20 13:27:17 +0200

    date and time

---
+++ Meghana/PYTHON/Conditional Statements and Date Time Module/Date&Time.py
@@ -0,0 +1,6 @@
+import datetime
+import calendar
+
+dateandtime = datetime.datetime.now()
+print("The current date and time is: ", dateandtime)
+print("The calendar of the year 2009 is shown below: ", calendar.calendar(2009))
