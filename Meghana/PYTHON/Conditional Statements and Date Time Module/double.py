927398a7505dd8805e95dffb3b42a43736fa8de4
MeghanaCH09 <meghanacheruvu@gmail.com>
2024-10-20 13:15:28 +0200

    double

---
+++ Meghana/PYTHON/Conditional Statements and Date Time Module/double.py
@@ -0,0 +1,11 @@
+number=int(input("Enter a number: "))
+
+if number>50:
+    print("The number is greater than 50")
+    if number%2==0:
+        print("The number is even")
+    else:
+        print("The number is odd")
+else: 
+    print("The number is less than 50")
+    
