2eb32e58d3e6a7d658f508430fa82115d9c1061a
MeghanaCH09 <meghanacheruvu@gmail.com>
2024-11-03 12:59:32 +0200

    lists

---
+++ Meghana/PYTHON/Data Structures in Python - 1/Operations in Lists.py
@@ -0,0 +1,18 @@
+fruit = []
+print(fruit)
+
+fruit = ['Mango', 'Pineapple', 'Apple', 'Strawberry', 'Plums', 'Grapes']
+print(fruit)
+
+fruit.append('Dragonfruit')
+print(fruit)
+fruit.remove("Apple")
+print(fruit)
+
+
+fruit.sort()
+print(fruit)
+fruit.reverse()
+print(fruit)
+fruit[0:4]
+print(fruit)
---
+++ Meghana/PYTHON/Intro To Turtle/star.py
@@ -0,0 +1,25 @@
+import turtle
+turtle.Screen().bgcolor("Blue")
+board=turtle.Turtle()
+sc = turtle.Screen()
+sc.setup(400,300)
+
+board.forward(100)
+board.left(120)
+board.forward(100)
+board.left(120)
+
+board.penup()
+board.right(150)
+board.forward(50)
+
+board.pendown()
+board.right(90)
+board.forward(100)
+board.left(120)
+board.forward(100)
+board.left(120)
+
+turtle.done()
+
+
