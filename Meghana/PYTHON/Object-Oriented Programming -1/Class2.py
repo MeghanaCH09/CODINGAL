f0cb3727fae7f4e82b68b7a27c53df5db68ffcb8
MeghanaCH09 <meghanacheruvu@gmail.com>
2024-11-16 13:00:19 +0200

    class2

---
+++ Meghana/PYTHON/Object-Oriented Programming -1/Class2.py
@@ -0,0 +1,13 @@
+class animal:
+    type = "Cat Family"
+    place = "Southeast Asia"
+
+    def intro(slef):
+        print("Hello, I am a Tiger")
+    def details(self):
+        print("I am part of the ", self.type)
+        print("I live in ", self.place)
+
+let=animal()
+let.intro()
+let.details()
