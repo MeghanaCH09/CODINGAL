c3a230476f88a6d5124a899e2fe95e8edfc2b146
MeghanaCH09 <meghanacheruvu@gmail.com>
2024-11-16 11:01:53 +0200

    tuple to list

---
+++ Meghana/PYTHON/Data Structures in Python - 2/ACP_Tuple To List.py
@@ -0,0 +1,6 @@
+personal_details = ("Meghana", "Cheruvu", 15, 5.4, 58, "Arts")
+
+details_list = list(personal_details)
+
+print("Original Tuple:", personal_details)
+print("Converted List:", details_list)
