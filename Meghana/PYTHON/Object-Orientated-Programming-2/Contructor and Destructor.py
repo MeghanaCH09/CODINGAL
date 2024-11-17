018744944e94d5b1c2aab19e7676854175498a29
MeghanaCH09 <meghanacheruvu@gmail.com>
2024-11-17 12:50:27 +0200

    constructor and destructor

---
+++ Meghana/PYTHON/Object-Orientated-Programming-2/Contructor and Destructor.py
@@ -0,0 +1,8 @@
+class student:
+
+    def __init__(self):
+        print("Student Details have been created by admin")
+    def __del__(self):
+        print("Student Details have been deleted by admin")
+
+obj=student()
