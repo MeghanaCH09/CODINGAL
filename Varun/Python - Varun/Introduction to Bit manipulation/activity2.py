c7bcb12948a8d4d3b361e2f010235c439371e178
MeghanaCH09 <meghanacheruvu@gmail.com>
2025-01-18 14:59:06 +0200

    Add files via upload

---
+++ Varun/Python - Varun/Introduction to Bit manipulation/activity2.py
@@ -0,0 +1,7 @@
+def checkevenorodd(n): 
+    if(n^1==n+1):
+        print("It is even")
+    else: 
+        print("It is odd")
+input1=int(input("Enter any number to find whether it is even or odd: "))
+checkevenorodd(input1)
